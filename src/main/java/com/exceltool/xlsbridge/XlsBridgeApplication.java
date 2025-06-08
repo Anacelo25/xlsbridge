83f45477d758bfbd0fad450f005f038bc5f5e239
Anacelo25 <marcelogemaque25@gmail.com>
2025-06-08 11:23:54 -0300

    commit test one

---
+++ src/main/java/com/exceltool/xlsbridge/XlsBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.exceltool.xlsbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlsBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XlsBridgeApplication.class, args);
+	}
+
+}
